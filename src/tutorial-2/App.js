2d0f63dd8e47d8082e260bcad3bb74eaae54a775
shpilson <kovalenko.jora@gmail.com>
2022-01-03 21:00:08 +0300

    Выполнил домашнее задание #2

--- src/tutorial-2/App.js
+++ src/tutorial-2/App.js
@@ -1,8 +1,39 @@
-function App() {
-    return (
-        <>
+function App(props) {
+    let email = '';
+    let password = '';
 
-        </>
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (email.length === 0 || password.length === 0) {
+
+            alert('Пожалуйста, заполните все поля')
+            return
+        }
+        console.log({ email, password });
+    }
+
+    function inputChange(e) {
+        const value = e.target.value.trim()
+
+        switch (e.target.name) {
+            case 'email':
+                email = value
+                break;
+            case 'password':
+                password = value
+                break;
+            default:
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name="email" onChange={inputChange} type="text" placeholder="E-mail" />
+            <br />
+            <input name="password" onChange={inputChange} type="password" placeholder="Пароль" />
+            <br />
+            <button>Войти</button>
+        </form>
     );
 }
 
