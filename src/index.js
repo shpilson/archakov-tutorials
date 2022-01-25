354e4f2a0c996f0b807ffa5a84b3ace41bd2e7b1
shpilson <kovalenko.jora@gmail.com>
2022-01-25 13:41:33 +0300

    Загрузил материалы для урока №3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './tutorial-1/App';
-import App from './tutorial-2/App';
+import App from './tutorial-3/App';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/tutorial-3/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1>Здесь что-то будет</h1>
+        </>
+    );
+}
+
+export default App;
