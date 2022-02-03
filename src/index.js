0eef387fabef3b427fb5df62eae25686372e9a6a
shpilson <kovalenko.jora@gmail.com>
2022-02-03 13:01:11 +0300

    Добавил модуль SASS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 // import App from './tutorial-1/App';
 // import App from './tutorial-2/App';
 // import App from './tutorial-3/App';
