82e7e5f2a554dff88e69d4bfd7c8cfa61bd6416e
shpilson <kovalenko.jora@gmail.com>
2022-02-05 13:39:11 +0300

    Some fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 // import App from './tutorial-1/App';
 // import App from './tutorial-2/App';
 // import App from './tutorial-3/App';
+//
 import App from './tutorial-5/App';
 
 ReactDOM.render(
