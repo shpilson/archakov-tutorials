5e9805befd75be94a1fc7361006fd833cbe43475
shpilson <kovalenko.jora@gmail.com>
2022-02-10 11:18:39 +0300

    Добавил React Router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.scss';
 // import App from './tutorial-1/App';
@@ -13,7 +14,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
